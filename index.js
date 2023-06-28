78ae0e63ef2d1e498f78e30f151bb4cc8be8bb92
minecraftteet <77221503+minecraftteet@users.noreply.github.com>
2023-06-28 09:12:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function casme(){
 }
 function go(){
     var input = document.getElementById("tom").value;
-    window.open('https://www.google.com/'+input,"_self');
+    window.open('https://www.google.com/'+input,"_self");
 }
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 13) {
